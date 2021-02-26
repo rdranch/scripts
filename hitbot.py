d8d0bcbb6a4774b6915dadadfa08e55e427c2609
Russell Dranch <rdranch275@gmail.com>
2021-02-26 12:45:38 -0500

    Hitbot rehaul
    There was some glitching where users would be added, the change would say it got reflected but it didn't actually. Hopefully the rehaul or 'cleanup' I did fixes this.

--- hitbot.py
+++ hitbot.py
@@ -4,24 +4,32 @@ from keep_alive import keep_alive
 client = discord.Client()
 
 def exists(user):
-  with open('list.txt') as f:
-      lines = f.readlines()
-      user = user + "\n"
-      if user in lines:
-        return True
-      return False
+  user = user + "\n"
+  with open('list.txt', 'r+') as f:
+    return user in f.readlines()
 
 def remove(user):
-  if exists(user):
-    with open('list.txt', 'r+') as f:
-        lines = f.readlines()
-        f.seek(0)
-        for i in lines:
-          if i.strip("\n") != user:
-            f.write(i)
-        f.truncate()
-  else:
-    return -1
+  with open('list.txt', 'r+') as f:
+    user = user + "\n"
+    lines = f.readlines()
+    f.seek(0)
+    f.truncate(0)
+    lines.remove(user)
+    f.writelines(lines)
+        
+
+def add(user):
+  user = user + "\n"
+  with open('list.txt', 'a+') as f:
+    f.write(user)
+
+def list():
+  msg = "Here is the list:\n"
+  with open('list.txt', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+      msg += line
+    return msg
 
 @client.event
 async def on_ready():
@@ -32,33 +40,24 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
-
-
   args = message.content.split()
   if args[0] == '.list':
-    msg = "Here is the list:\n"
-    with open('list.txt') as f:
-      lines = f.readlines()
-      for line in lines:
-        msg += line
-      await message.channel.send(msg)
+    msg = list()
+    await message.channel.send(msg)
   elif args[0] == ".help":
     await message.channel.send("Here are the commands:\n.help\n.list\n.add\n.remove")
   elif args[0] == '.add' and len(args) == 2:
     if exists(args[1]):
       await message.channel.send("This user is already in the list.")
     else:
-      f = open("list.txt", "a+")
-      f.write(args[1]+"\n")
-      f.close()
+      add(args[1])
       await message.channel.send("{user} has been added to the list.".format(user=str(args[1])))
   elif args[0] == '.remove' and len(args) == 2:
-    if remove(str(args[1])) == -1:
-      await message.channel.send("User is not in the list.")
-      pass
+    if exists(args[1]):
+      remove(args[1])
+      await message.channel.send("{user} has been removed from the list.".format(user=str(args[1])))
     else:
-      remove(str(args[1]))
-      await message.channel.send("{user} has been removed from the list.".format(user=str(args[1]))) 
+      await message.channel.send("User is not in the list.")
   elif args[0][0] == ".":
     await message.channel.send("Incorrect arguments.")
   else:
@@ -66,4 +65,4 @@ async def on_message(message):
 
 keep_alive()
 
-client.run(' {redacted} ')
+client.run(' {redcated} ')
