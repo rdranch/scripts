8f2c120b31b756dd821f5cc3524ecc55e69381ad
Russell Dranch <rdranch275@gmail.com>
2021-03-07 15:21:24 -0500

    Custom B64 Decoder
    Replace "custom" with whatever custom library you want to use. Then replace the encrypted string with whatever you want to decode.

--- hitbot.py
+++ hitbot.py
@@ -4,32 +4,24 @@ from keep_alive import keep_alive
 client = discord.Client()
 
 def exists(user):
-  user = user + "\n"
-  with open('list.txt', 'r+') as f:
-    return user in f.readlines()
+  with open('list.txt') as f:
+      lines = f.readlines()
+      user = user + "\n"
+      if user in lines:
+        return True
+      return False
 
 def remove(user):
-  with open('list.txt', 'r+') as f:
-    user = user + "\n"
-    lines = f.readlines()
-    f.seek(0)
-    f.truncate(0)
-    lines.remove(user)
-    f.writelines(lines)
-        
-
-def add(user):
-  user = user + "\n"
-  with open('list.txt', 'a+') as f:
-    f.write(user)
-
-def list():
-  msg = "Here is the list:\n"
-  with open('list.txt', 'r') as f:
-    lines = f.readlines()
-    for line in lines:
-      msg += line
-    return msg
+  if exists(user):
+    with open('list.txt', 'r+') as f:
+        lines = f.readlines()
+        f.seek(0)
+        for i in lines:
+          if i.strip("\n") != user:
+            f.write(i)
+        f.truncate()
+  else:
+    return -1
 
 @client.event
 async def on_ready():
@@ -40,33 +32,37 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
+
+
   args = message.content.split()
   if args[0] == '.list':
-    msg = list()
-    await message.channel.send(msg)
+    msg = "Here is the list:\n"
+    with open('list.txt') as f:
+      lines = f.readlines()
+      for line in lines:
+        msg += line
+      await message.channel.send(msg)
   elif args[0] == ".help":
-    await message.channel.send("Here are the commands:\n.help\n.list\n.add\n.remove\n.search")
+    await message.channel.send("Here are the commands:\n.help\n.list\n.add\n.remove")
   elif args[0] == '.add' and len(args) == 2:
     if exists(args[1]):
       await message.channel.send("This user is already in the list.")
     else:
-      add(args[1])
+      f = open("list.txt", "a+")
+      f.write(args[1]+"\n")
+      f.close()
       await message.channel.send("{user} has been added to the list.".format(user=str(args[1])))
   elif args[0] == '.remove' and len(args) == 2:
-    if exists(args[1]):
-      remove(args[1])
-      await message.channel.send("{user} has been removed from the list.".format(user=str(args[1])))
-    else:
+    if remove(str(args[1])) == -1:
       await message.channel.send("User is not in the list.")
-  elif args[0] == '.search' and len(args) == 2:
-    if exists(args[1]):
-      await message.channel.send("{user} is in the list.".format(user=str(args[1])))
+      pass
     else:
-      await message.channel.send("{user} is not in the list.".format(user=str(args[1])))
+      remove(str(args[1]))
+      await message.channel.send("{user} has been removed from the list.".format(user=str(args[1]))) 
   elif args[0][0] == ".":
     await message.channel.send("Incorrect arguments.")
   else:
-    await message.delete()
+    pass
 
 keep_alive()
 
