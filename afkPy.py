433720d89c87720142dc187a552e0cdcc581b0d5
Russell Dranch <rdranch275@gmail.com>
2021-10-14 01:38:45 -0400

    Update afkPy.py
    Beta version of bot. Adds randomization and crash support.

--- afkPy.py
+++ afkPy.py
@@ -1,8 +1,10 @@
 import win32api, keyboard, multiprocessing, ctypes
 from json import dumps, loads
 from time import sleep
-from os import path, getcwd
-from random import randint, uniform
+from os import path, getcwd, startfile
+from random import randint, uniform, shuffle
+from win32gui import GetWindowText, GetForegroundWindow
+from psutil import process_iter
 
 # TODO add randomization to looking around
 
@@ -26,120 +28,163 @@ default_config = {
 cx = win32api.GetSystemMetrics(0) # Get screen width
 cy = win32api.GetSystemMetrics(1) // 2 # Get screen height
 
-def macro(config): 
+def macro(config, path):
+    '''Main macro script which will run a series of actions found in the config file to automatically move.
+    Will infinitely run until forced shut or process is terminated.
+
+    Arguments
+    ---------
+    Config : Dictionary
+        Key/action mapping
+    Path : string
+        The path of R6 executable
+    '''
+    
+    # Checks if Rainbow Six is open, will restart it if not.
     while True:
-        # Reload --> Up --> Lean left --> Lean Right --> Straighten out --> Left --> Down --> Right --> Crouch 
-        keyboard.press(config["reload"])
-        sleep(round(uniform(0.3, 1), 2))
-        keyboard.release(config["reload"])
-        sleep(round(uniform(0.3, 1), 2))
-        keyboard.press(config["up"])
-        sleep(round(uniform(0.3, 1), 2))
-        keyboard.release(config["up"])
-        sleep(round(uniform(0.3, 1), 2))
-        keyboard.press(config["left"])
-        sleep(round(uniform(0.3, 1), 2))
-        keyboard.release(config["left"])
-        sleep(round(uniform(0.3, 1), 2))
-        keyboard.press(config["down"])
-        sleep(round(uniform(0.3, 1), 2))
-        keyboard.release(config["down"])
-        sleep(round(uniform(0.3, 1), 2))
-        keyboard.press(config["right"])
-        sleep(round(uniform(0.3, 1), 2))
-        keyboard.release(config["right"])
-        sleep(round(uniform(0.3, 1), 2))
-        keyboard.press(config["crouch"])
-        sleep(round(uniform(0.3, 1), 2))
-        keyboard.release(config["crouch"])
-        sleep(round(uniform(0.3, 1), 2))
+        if "RainbowSix.exe" not in [p.name() for p in process_iter()]:
+            print("[!] Siege crashed or closed. Stopping bot while it restarts.")
+            startfile(path)
+            sleep(120)
+            print("[*] Continuing...")
+            keyboard.press('enter')
+            sleep(0.03)
+            keyboard.release('enter')
+
+        # Shuffles order in which actions are done
+        random_list = ["reload", "up", "left", "down", "right", "crouch"]
+        shuffle(random_list)
+
+        # Main activity section
+        keyboard.press(config[random_list[0]])
+        sleep(uniform(0.3, 1))
+        keyboard.release(config[random_list[0]])
+        sleep(uniform(0.3, 1))
+        keyboard.press(config[random_list[1]])
+        sleep(uniform(0.3, 1))
+        keyboard.release(config[random_list[1]])
+        sleep(uniform(0.3, 1))
+        keyboard.press(config[random_list[2]])
+        sleep(uniform(0.3, 1))
+        keyboard.release(config[random_list[2]])
+        sleep(uniform(0.3, 1))
+        keyboard.press(config[random_list[3]])
+        sleep(uniform(0.3, 1))
+        keyboard.release(config[random_list[3]])
+        sleep(uniform(0.3, 1))
+        keyboard.press(config[random_list[4]])
+        sleep(uniform(0.3, 1))
+        keyboard.release(config[random_list[4]])
+        sleep(uniform(0.3, 1))
+        keyboard.press(config[random_list[5]])
+        sleep(uniform(0.3, 1))
+        keyboard.release(config[random_list[5]])
+        sleep(uniform(0.3, 1))
         keyboard.press(config["lean_left"])
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         keyboard.release(config["lean_left"])
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         keyboard.press(config["lean_right"])
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         keyboard.release(config["lean_right"])
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         keyboard.press(config["lean_right"])
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         keyboard.release(config["lean_right"])
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
 
         # ADS/UNAds
         for _ in range(2):
             win32api.mouse_event(0x0008, 0, 0, 0, 0) # Right click press
-            sleep(round(uniform(0.3, 1), 2))
+            sleep(uniform(0.3, 1))
             win32api.mouse_event(0x0010, 0, 0, 0, 0) # Right click release
-            sleep(round(uniform(0.3, 1), 2))
+            sleep(uniform(0.3, 1))
 
         # Sit down / stand up
         for _ in range(2):
             keyboard.press(config["prone"])
-            sleep(round(uniform(0.3, 1), 2))
+            sleep(uniform(0.3, 1))
             keyboard.release(config["prone"])
-            sleep(round(uniform(0.5, 1), 2))
+            sleep(uniform(0.5, 1))
         
         # Drop/pickup bomb, ping
         keyboard.press(config["interact"])
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         keyboard.release(config["interact"])
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         keyboard.press(config["ping"])
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         keyboard.release(config["ping"])
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         
         # Pull/put out gadget
         keyboard.press(config["gadget"])
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.6, 1))
         win32api.mouse_event(0x0800, 0, 0, 120, 0) # SCROLL UP - ONE WHEEL CLICK IS 120
         keyboard.release(config["gadget"])
-        sleep(1.0)
+        sleep(1.5)
 
         win32api.mouse_event(0x0001, 0, -1000, 0, 0) # Moves cursor to top of screen
         sleep(0.5)
 
         win32api.mouse_event(0x0002, 0, 0, 0, 0) # Left click press
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         win32api.mouse_event(0x0004, 0, 0, 0, 0) # Left click release
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
 
         win32api.mouse_event(0x0020, 0, 0, 0, 0) # Middle click press
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         win32api.mouse_event(0x0040, 0, 0, 0, 0) # Middle click release
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
 
         win32api.mouse_event(0x0001, 0, cy, 0 ,0) # Centers crosshair
         keyboard.press('enter')
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         keyboard.press('enter')
         sleep(0.03)
         keyboard.release('enter')
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         keyboard.release('enter')
         sleep(0.03)
 
         # Spin 4x to the right
         for _ in range(4):
-            x, y = win32api.GetCursorPos()
             win32api.mouse_event(0x0001, cx//4, 0, 0, 0)
-            sleep(round(uniform(0.3, 1), 2))
+            sleep(uniform(0.3, 1))
 
         keyboard.press('esc')
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
         keyboard.release('esc')
         sleep(0.03)
         keyboard.press('esc')
         sleep(0.03)
         keyboard.release('esc')
-        sleep(round(uniform(0.3, 1), 2))
+        sleep(uniform(0.3, 1))
+
     
+def check_open():
+    '''Checks all process names to see if RainbowSix is open
+    If R6 is found, it will return the path.
+
+    Returns
+    -------
+    String : executable path
+    '''
+    print("[*] Waiting for Rainbow Six to be opened.")
+    while "RainbowSix.exe" not in [p.name() for p in process_iter()]:
+        sleep(2)
+    
+    for proc in process_iter():
+        if proc.name() == "RainbowSix.exe":
+            return proc.exe()
+
 
 if __name__ == "__main__":
-    ctypes.windll.kernel32.SetConsoleTitleW("Bot coded by Darkon (Darkinator#3932) AKA The.Don_ (v.0.2)")
+    # Create title for exe
+    ctypes.windll.kernel32.SetConsoleTitleW("Bot coded by Darkon (Darkinator#3932) AKA The.Don_ (v.1.0 BETA)")
     multiprocessing.freeze_support()
 
+    # Check if config file is created
     if not path.exists("botSettings.ini"):   
         with open("botSettings.ini", "w") as f:
             f.write(dumps(default_config, indent=4))
@@ -150,12 +195,14 @@ if __name__ == "__main__":
             config = loads(f.read())
     toggle = config['toggle']
 
+    # Run bot
     try:
+        path = check_open()
         print(f"[*] Press {toggle} to activate/deactive bot. CTRL+C to EXIT.") 
         while True:
             if keyboard.is_pressed(toggle):
                 print("[*] BOT ACTIVATED") 
-                proc = multiprocessing.Process(target=macro, args=(config,))
+                proc = multiprocessing.Process(target=macro, args=(config, path))
                 proc.start()
                 keyboard.wait(toggle)
                 print("[*] BOT DEACTIVATED") 
